58f647c9a374749d18ee67063a622632a03f47d9
gustav0-gg <gugugarciaaa+github@gmail.com>
2025-09-17 17:33:30 -0300

    fix (#1): Ajuste nas colunas da db
    Ajustando as colunas de acordo com os requisitos do cliente

--- resources/schema.sql
+++ resources/schema.sql
@@ -4,17 +4,21 @@ USE sistema_pdi;
 CREATE TABLE usuarios (
 	id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(100) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    funcao ENUM('RH', 'GESTOR_GERAL', 'GESTOR_AREA') NOT NULL
+    cpf VARCHAR(11) UNIQUE NOT NULL,
+    data_nascimento DATE NOT NULL,
+    funcao ENUM('RH', 'GESTOR_GERAL', 'GESTOR_AREA') NOT NULL,
+    experiencia TEXT,
+    observacoes TEXT
 );
 
 CREATE TABLE colaboradores (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+	id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(100) NOT NULL,
-    email VARCHAR(100), 
-    cargo VARCHAR(100),
-    setor VARCHAR(100),
-    data_admissao DATE
+    cpf VARCHAR(11) UNIQUE NOT NULL,
+    data_nascimento DATE NOT NULL,
+	cargo VARCHAR(50) NOT NULL,
+    experiencia TEXT,
+    observacoes TEXT
 );
 
 CREATE TABLE pdis (
