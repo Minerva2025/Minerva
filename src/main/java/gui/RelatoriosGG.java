57e078c3409fb55a016a6787102f39a432f4d402
Guilherme Rosa <guilherme_rozza@hotmail.com>
2025-11-20 19:34:22 -0300

    feat(#43): pagina de relatorios Gestor Geral. Parte 1
    Adicionando tanto o gráfico de barras, quanto o de pizza a pagina de relatórios do gestor geral, ainda sem estilização.

--- src/main/java/gui/RelatoriosGG.java
+++ src/main/java/gui/RelatoriosGG.java
@@ -1,38 +1,48 @@
 package gui;
 
+import dao.ColaboradorDAO;
+import dao.PdiDAO;
 import javafx.application.Application;
 import javafx.beans.binding.Bindings;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.chart.*;
 import javafx.scene.effect.GaussianBlur;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.shape.Ellipse;
 import javafx.scene.text.Text;
-import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 
-
+import model.Colaborador;
+import model.Pdi;
+import model.Status;
 import model.Usuario;
 
-
+import java.util.List;
 
 
 public class RelatoriosGG extends Application {
 
+    PdiDAO pdiDAO = new PdiDAO();
+    ColaboradorDAO colaboradorDAO = new ColaboradorDAO();
+    List<Colaborador> colaboradores = colaboradorDAO.listAll();
+
+
     private Usuario logado;
-
     public RelatoriosGG(Usuario usuariologado) {this.logado = usuariologado;}
-
 
     @Override
     public void start(Stage relatoriosggStage) {
-        // Objeto Barra Lateral
+        // Criação da Barra Lateral
         BarraLateralGG barra = new BarraLateralGG(logado);
 
+        // VBox center
         VBox center = new VBox();
         center.setId("relatoriosgg");
         center.setAlignment(Pos.TOP_CENTER);
@@ -40,10 +50,20 @@ public class RelatoriosGG extends Application {
         center.setPadding(new Insets(15));
         center.setStyle("-fx-background-color: #1E1E1E;");
 
+        // VBox titulo
+        VBox boxTitulo = new VBox();
+        boxTitulo.setId("boxTitulo");
+        boxTitulo.setAlignment(Pos.TOP_LEFT);
+        boxTitulo.setSpacing(15);
+        boxTitulo.setPadding(new Insets(30));
+
         Text titulo = new Text("Relatórios de PDIs");
         titulo.setId("titulo");
         VBox.setMargin(titulo, new Insets(4, 0, 30, 0));
-        
+
+        boxTitulo.getChildren().add(titulo);
+
+        // Blobs
         Ellipse blob1 = new Ellipse();
         blob1.setId("blob1");
         
@@ -58,13 +78,92 @@ public class RelatoriosGG extends Application {
 		blob2.setEffect(blur);
 		blob3.setEffect(blur);
 
-        center.getChildren().addAll(titulo);
+        // BarChart
+
+        VBox boxBarchart = new VBox();
+
+        CategoryAxis xAxis = new CategoryAxis();
+        xAxis.setLabel("Setor");
+
+        NumberAxis yAxis = new NumberAxis();
+        yAxis.setLabel("Evolução");
+
+        BarChart<String, Number> barChart = new BarChart<>(xAxis, yAxis);
+        barChart.setTitle("Evolução das metas por setor.");
+        barra.setId("barchart");
+
+        XYChart.Series<String, Number> serieConcluido = new XYChart.Series<>();
+        serieConcluido.setName("Concluido");
+        XYChart.Series<String, Number> serieAndamento = new XYChart.Series<>();
+        serieAndamento.setName("Em andamento");
+        XYChart.Series<String, Number> serieNaoIniciado = new XYChart.Series<>();
+        serieNaoIniciado.setName("Não iniciado");
+        XYChart.Series<String, Number> serieAtrasado = new XYChart.Series<>();
+        serieAtrasado.setName("Atrasado");
+
+        int totalConcluido = 0;
+        int totalAndamento = 0;
+        int totalNaoIniciado = 0;
+        int totalAtrasado = 0;
+
+        for(Colaborador colaborador : colaboradores){
+            List<Pdi> listaDePdis = pdiDAO.findByColaborador(colaborador.getId());
+
+            int totalConcluidoPorColaborador = (int) listaDePdis.stream()
+                    .filter(pdi -> pdi.getStatus() == Status.CONCLUIDO)
+                    .count();
+            totalConcluido = totalConcluido + totalConcluidoPorColaborador;
+
+            int totalAndamentoPorColaborador = (int) listaDePdis.stream()
+                    .filter(pdi -> pdi.getStatus() == Status.EM_ANDAMENTO)
+                    .count();
+            totalAndamento = totalAndamento + totalAndamentoPorColaborador;
+
+            int totalNaoIniciadoPorColaborador = (int) listaDePdis.stream()
+                    .filter(pdi -> pdi.getStatus() == Status.NAO_INICIADO)
+                    .count();
+            totalNaoIniciado = totalNaoIniciado + totalNaoIniciadoPorColaborador;
+
+            int totalAtrasadoPorColaborador = (int) listaDePdis.stream()
+                    .filter(pdi -> pdi.getStatus() == Status.ATRASADO)
+                    .count();
+            totalAtrasado = totalAtrasado + totalAtrasadoPorColaborador;
+        }
+
+        for(Colaborador colaborador : colaboradores){
+            serieConcluido.getData().add(new XYChart.Data<>(colaborador.getSetor(),totalConcluido));
+            serieAndamento.getData().add(new XYChart.Data<>(colaborador.getSetor(),totalAndamento));
+            serieNaoIniciado.getData().add(new XYChart.Data<>(colaborador.getSetor(), totalNaoIniciado));
+            serieAtrasado.getData().add(new XYChart.Data<>(colaborador.getSetor(), totalAtrasado));
+        }
+
+        barChart.getData().addAll(serieConcluido,serieAndamento,serieNaoIniciado,serieAtrasado);
+        boxBarchart.getChildren().add(barChart);
+
+        // PieChart
+
+        VBox boxPieChart = new VBox();
+
+        ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList(
+                new PieChart.Data("Metas em andamento", totalAndamento),
+                new PieChart.Data("Metas atrasadas", totalAtrasado)
+        );
+
+        PieChart pieChart = new PieChart(pieChartData);
+
+        boxPieChart.getChildren().add(pieChart);
+
+        // Graficos HBox
+
+        HBox graficos = new HBox();
+
+        graficos.getChildren().addAll(boxBarchart,boxPieChart);
+
+        // AddAll do Center
+        center.getChildren().addAll(boxTitulo,graficos);
 
 
-
-
-
-
+        // HBox root
         HBox root = new HBox();
         root.setStyle("-fx-background-color: #1E1E1E");
         root.getChildren().addAll(barra, center, blob1, blob2, blob3);
